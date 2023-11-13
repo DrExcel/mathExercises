227f879ab45e2bcafaab3f5f49642598b3e11d6d
DrExcel <thorsten.wingenroth@dhbw-stuttgart.de>
2023-11-13 11:51:43 +0100

    Update pointsFew.js

--- pointsFew.js
+++ pointsFew.js
@@ -3,4 +3,4 @@
 	const moodleStg = 'elearning.dhbw-stuttgart.de';
 	if (location.host == moodleStg) moodle = true; else moodle = false;
 	const moodleId = '341411'; // ID der Moodle-redirect-Seite
-	// Update 23-03-12 at 4:33pm
+	// Update 23-11-13 at 11:51pm
