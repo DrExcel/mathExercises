9a1facb797d222f1bd455f1ccd92b7596133afdf
DrExcel <thorsten.wingenroth@dhbw-stuttgart.de>
2025-10-30 15:22:16 +0100

    Update pointsFew.js
    Update für WBK25

--- pointsFew.js
+++ pointsFew.js
@@ -2,5 +2,6 @@
 	var moodle;
 	const moodleStg = 'elearning.dhbw-stuttgart.de';
 	if (location.host == moodleStg) moodle = true; else moodle = false;
-	const moodleId = '402187'; // ID der Moodle-redirect-Seite
+	const moodleId = '465578'; // ID der Moodle-redirect-Seite
 	// Update 23-11-13 at 11:51pm
+
